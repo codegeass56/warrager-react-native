4174713d50bac51c13566212a3c5ccc69c8a0a04
Siddharth Panjwani <siddharthkpanjwani@gmail.com>
2025-12-12 00:11:58 +0400

    chore: Reorder imports and config attributes
    - Reorders with prettier.
    - Updates config description comment.

--- firebaseConfig.ts
+++ firebaseConfig.ts
@@ -1,22 +1,23 @@
+import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
 import { initializeApp } from "firebase/app";
 // @ts-ignore
-import { initializeAuth, getReactNativePersistence } from "firebase/auth";
-import ReactNativeAsyncStorage from "@react-native-async-storage/async-storage";
+import { getReactNativePersistence, initializeAuth } from "firebase/auth";
 import { getDatabase } from "firebase/database";
 import { getStorage } from "firebase/storage";
 
-// Initialize Firebase
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDUnjz3QSbXbNyZOrsGI-WkvyAXr3mMpIk",
   authDomain: "warrager-2bb2e.firebaseapp.com",
+  databaseURL:
+    "https://warrager-2bb2e-default-rtdb.asia-southeast1.firebasedatabase.app",
   projectId: "warrager-2bb2e",
   storageBucket: "warrager-2bb2e.appspot.com",
   messagingSenderId: "473374145367",
   appId: "1:473374145367:web:a6df46528034e724a75c12",
-  databaseURL:
-    "https://warrager-2bb2e-default-rtdb.asia-southeast1.firebasedatabase.app/",
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
